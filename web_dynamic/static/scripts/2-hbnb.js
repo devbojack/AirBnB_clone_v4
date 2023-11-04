b17f3e659b77e61cb9de27f37432a11b6d3c32e4
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-05 00:49:56 +0300

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,6 +3,20 @@ $(document).ready(function () {
   const $amenitiesCheck = $('input[type=checkbox]');
   const $selectedAmenities = $('div.amenities h4');
 
+  function updateApiStatusClass(status){
+    if (status == 'OK'){
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  }
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    const status = data.status;
+
+    updateApiStatusClass(status);
+});
+
   $amenitiesCheck.click(function () {
     if ($(this).is(':checked')) {
       dict[$(this).data('id')] = $(this).data('name');
