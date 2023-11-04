88f387a9bb611dbfbf7892f3a45005f42fd0e639
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-04 23:44:15 +0300

    amenities

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,13 +7,11 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+from uuid import uuid4
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
-def generate_cache_id():
-    """Generate a UUID"""
-    return str(uuid.uuid4())
 
 @app.teardown_appcontext
 def close_db(error):
@@ -21,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,13 +35,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    cache_id = generate_cache_id()
-
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,2 +1,21 @@
-t
+$(document).ready(function(){
+    var amenityIds = [];
+
+    $('input[type="checkbox"]').change(function(){
+        var amenityId = $(this).data('id');
+
+        if ($(this).is(':checked')){
+            amenityIds.push(amenityId);
+        }
+        else {
+            var index = amenityIds.indexOf(amenityId);
+            if (index > -1) {
+                amenityIds.splice(index, 1);
+            }
+        }
+
+        var amenitiesText = amenityIds.join(', ');
+        $('#amenities h4').text(amenitiesText);
+    });
+});
 
